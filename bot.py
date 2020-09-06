5f8d1ba1402670a8cde0597bb23b758b6ade1c9b
Sektor <krzysiek1230@gmail.com>
2020-09-06 19:06:41 +0200

    Init commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+import os
+import random
+
+import discord
+from discord.errors import LoginFailure
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the server!'
+    )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+try:
+    client.run(TOKEN)
+except LoginFailure as e:
+    print(f'Error: {e}')
